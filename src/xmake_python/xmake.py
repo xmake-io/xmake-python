9fc022886e8f5c3065c387323b1bb726a666ca70
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-05-24 05:27:49 +0800

    :adhesive_bandage: Add -y for xmake

--- src/xmake_python/xmake.py
+++ src/xmake_python/xmake.py
@@ -22,15 +22,15 @@ class XMaker:
         cmd = [
             self.xmake,
             "config",
-            "-P",
+            "-yP",
             self.tempname,
         ] + split(self.command)
         run(cmd)
 
     def build(self):
-        cmd = [self.xmake, "-y", "-P", self.tempname, "--verbose"]
+        cmd = [self.xmake, "-yP", self.tempname, "--verbose"]
         run(cmd)
 
     def install(self):
-        cmd = [self.xmake, "install", "-P", self.tempname, "-o", self.tempname]
+        cmd = [self.xmake, "install", "-yP", self.tempname, "-o", self.tempname]
         run(cmd)
