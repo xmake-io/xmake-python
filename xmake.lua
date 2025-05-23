585fb4a093faa8ea886b51e1b50fda9e7a2f8524
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-05-24 01:10:00 +0800

    :pencil2: Fix a typo

--- xmake.lua
+++ xmake.lua
@@ -3,5 +3,5 @@ do
     set_kind("phony")
     add_installfiles("src/xmake_python/*.py", { prefixdir = "$(xmake-platlib)/xmake_python" })
     add_installfiles("src/xmake_python/builder/*.py", { prefixdir = "$(xmake-platlib)/xmake_python/builder" })
-    add_installfiles("src/xmake_python/template/*", { prefixdir = "$(xmake-platlib)/xmake_python/template" })
+    add_installfiles("src/xmake_python/templates/*", { prefixdir = "$(xmake-platlib)/xmake_python/templates" })
 end
