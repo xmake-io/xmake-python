1c219fee5c5af73f64ed7f67e36dfdad1471d89b
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-06-19 01:00:05 +0800

    :rewind: Revert ":adhesive_bandage: Search xmake.cmd for Windows NT"
    This reverts commit 0b8a7909faa2c4b0b774694cf56a14832a4a2439.

--- src/xmake_python/wheel.py
+++ src/xmake_python/wheel.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from types import SimpleNamespace
 import zipfile
 from pathlib import Path
-from shutil import which
 
 from . import common
 from .templates import __version__
@@ -121,10 +120,7 @@ class WheelBuilder:
         make_path = directory / maker.get("makefile", "Makefile")
         build_system = get_build_system(xmake_path, make_path, configure_path, configure_ac_path)
         if build_system == "xmake":
-            ext = ""
-            if os.name == "nt" and which("xmake") is None:
-                ext = ".cmd"
-            xmake = XMaker(xmaker.get("xmake", "xmake" + ext),
+            xmake = XMaker(xmaker.get("xmake", "xmake"),
                            xmaker.get("command", ""),
                            xmaker.get("tempname", ""),
                            xmaker.get("project", os.path.abspath(".")),
